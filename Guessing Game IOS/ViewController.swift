436d23b05bb5dc85d34f86cd1dcfe6cf720934f1
Miranda Jessie <mirandajessie@MacBook-Pro-27.local>
2018-10-10 10:03:57 -0500

    Added code for the VeiwController and connecting the UI to the code.

--- Guessing Game IOS/ViewController.swift
+++ Guessing Game IOS/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var GuessTextField: UITextField!
+    @IBOutlet weak var GuessButton: UIButton!
+    @IBOutlet weak var FeedbackLabel: UILabel!
+    @IBOutlet weak var GuessesLeftLabel: UILabel!
+    
+    @IBAction func GuessButtonTapped(_ sender: Any) {
+        guesses -= 1
+        GuessesLeftLabel.text = "\(guesses) guesses remaining"
+    }
+    
+    let randomNumber = Int.random(in: 1...100)
+    var guesses = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
